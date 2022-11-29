b8768e38fd409587b0271370e7403235c7a3992d
NYAHA_1003 <hsk5stars@gmail.com>
2022-11-29 17:55:50 +0900

    1129_4
    적 소환 기초

--- Kimhong3/Assets/Scripts/Enemy/EnemyBehavior.cs
+++ Kimhong3/Assets/Scripts/Enemy/EnemyBehavior.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    float minXPos;
+    float minYPos;
+    float maxXPos;
+    float maxYPos;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,24 @@ public class EnemyBehavior : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void Move()
+    {
+
+    }
+
+    private void Attack()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            //¿Ã∆Â∆Æ ø¨√‚
+            Destroy(gameObject);
+        }
     }
 }
