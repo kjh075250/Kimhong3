ac9f5d6c90098e88db2cc47ee81702e1736e390b
NYAHA_1003 <hsk5stars@gmail.com>
2022-11-29 15:01:27 +0900

    1129_2
    Enemy, EnemyBehavior, EnemyFSM 틀 추가

---
+++ Kimhong3/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy
+{
+    int hp;
+    int speed;
+
+    public void Move() { }
+    public void Attack() { }
+}
